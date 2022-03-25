5095cf7923a20d586f1b1faa9d5098ce8e752fb6
vahidselimov <100331397+vahidselimov@users.noreply.github.com>
2022-03-25 13:10:00 +0400

    task

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Isodd(12));
+            Console.WriteLine(Iseven(13));
+
+        }
+        public static bool Isodd(int num)
+        {
+            if (num % 2 == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+
+            }
+
+
+
+        }
+    }
+    public static bool Iseven (int num2)
+    {
+        if (num2%2==0)
+        {
+            return true;
+
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+
+}
+
+
+
+
+
+
+
